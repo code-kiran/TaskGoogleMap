f6cb1a02e716a378b8d9a7774e5251c9c5119764
kiran <kiran@kirans-pc.local>
2018-09-10 19:59:21 +0545

    Initial Commit

---
+++ TaskGoogleMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskGoogleMap
+//
+//  Created by kiran on 9/10/18.
+//  Copyright © 2018 kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
